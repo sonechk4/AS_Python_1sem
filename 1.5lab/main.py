ed0c8f49c4865fa4882e553e4ff719abed445eb8
sonechk4 <sofya.pogodina.2602@gmail.com>
2025-11-01 22:42:05 +0300

    Update main.py

--- 1.5lab/main.py
+++ 1.5lab/main.py
@@ -1,2 +1,8 @@
-if __name__ == "__main__":
-    pass
+# Дан список. Выбрать из списка все числа на нечетных позициях и упорядочить эти числа по убыванию.
+import numpy as np
+spisok = np.arange(1, 100)
+res = []
+for i in range(len(spisok)):
+  if i%2 != 0:
+    res.append(int(spisok[i]))
+print(sorted(res, reverse=True))
