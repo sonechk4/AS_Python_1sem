636da30b5d43b39acea97ba4c41746ba644e9699
sonechk4 <sofya.pogodina.2602@gmail.com>
2025-11-01 22:40:32 +0300

    Update main.py

--- 1.3lab/main.py
+++ 1.3lab/main.py
@@ -1,2 +1,8 @@
-if __name__ == "__main__":
-    pass
+# Для заданного натурального n и действительного x подсчитать сумму: 1 + x + x^2/2! + x^3/3! + ... + x^n/n!
+import math
+n = int(input())
+x = float(input())
+summ = 1
+for k in range(1, n+1):
+  summ += (x**k / math.factorial(k))
+print(summ)
