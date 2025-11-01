58a78740f862ad91497edfffd4eda53564eef93d
sonechk4 <sofya.pogodina.2602@gmail.com>
2025-11-01 22:41:17 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,2 +1,10 @@
-if __name__ == "__main__":
-    pass
+# Дан список. Найти два соседних элемента, сумма которых максимальна, и вывести эти элементы в порядке возрастания их индексов.
+import numpy as np
+spisok = np.arange(1, 100)
+maxs = 0
+ind = 0
+for i in range(len(spisok)-1):
+  if spisok[i] + spisok[i+1] >= maxs:
+    maxs = spisok[i] + spisok[i+1]
+    ind = i
+print(spisok[i], spisok[i+1])
