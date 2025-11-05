a12b168f692f059373127d5fc79be84d8b9471dd
sonechk4 <sofya.pogodina.2602@gmail.com>
2025-11-05 20:49:05 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -2,9 +2,7 @@
 import numpy as np
 spisok = np.arange(1, 100)
 maxs = 0
-ind = 0
 for i in range(len(spisok)-1):
   if spisok[i] + spisok[i+1] >= maxs:
     maxs = spisok[i] + spisok[i+1]
-    ind = i
 print(spisok[i], spisok[i+1])
