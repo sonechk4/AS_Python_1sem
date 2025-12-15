2909377fe3481ff7a97fd49d5da6dd8a62c5b632
sonechk4 <sofya.pogodina.2602@gmail.com>
2025-12-15 21:50:50 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,13 @@
-if __name__ == "__main__":
-    pass
+def Chessboard(m, n):
+    res = []
+    for x in range(m):
+        row = []
+        for y in range(n):
+            if (x+y)%2 == 0:
+                row.append(0)
+            else:
+                row.append(1)
+        res.append(row)
+    return res
+matrix = Chessboard(3, 5)
+print(matrix)
