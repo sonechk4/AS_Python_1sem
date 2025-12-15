f52641cc0ecc96e11df5ff89fb5fc784c82cd7d8
sonechk4 <sofya.pogodina.2602@gmail.com>
2025-12-15 21:56:05 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,8 @@
-if __name__ == "__main__":
-    pass
+with open('f.txt', 'r') as f, open('g.txt', 'w') as g, open('h.txt', 'w') as h:
+    numbers = f.read().split()
+    for num in numbers:
+        n = int(num)
+        if n % 2 == 0:
+            g.write(num + ' ')
+        else:
+            h.write(num + ' ')
