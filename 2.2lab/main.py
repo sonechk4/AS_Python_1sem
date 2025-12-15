a63bdf5dd80c15c4cc0066aebb5d3cc11f4ec010
sonechk4 <sofya.pogodina.2602@gmail.com>
2025-12-15 21:50:15 +0300

    Update main.py

--- 2.2lab/main.py
+++ 2.2lab/main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    pass
+def LuckyNum(a):
+    if (int(str(a)[0]) + int(str(a)[1])) == (int(str(a)[2]) + int(str(a)[3])):
+        return True
+
+for i in range(1000, 10000):
+    if LuckyNum(i) == True:
+        print(i)
