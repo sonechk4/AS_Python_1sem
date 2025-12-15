5044b5760c87a35a5ecea233a61c25f767f05bcc
sonechk4 <sofya.pogodina.2602@gmail.com>
2025-12-15 21:49:43 +0300

    Update main.py

--- 2.1lab/main.py
+++ 2.1lab/main.py
@@ -1,2 +1,22 @@
-if __name__ == "__main__":
-    pass
+"Пункт а"
+keys = ['a', 'b', 'c']
+values = [1, 2, 3]
+res = dict(zip(keys, values))
+
+"Пункт б"
+items = [{'name': 'item1', 'price': 10},
+         {'name': 'item2', 'price': 800},
+         {'name': 'item3', 'price': 90},
+         {'name': 'item4', 'price': 78}
+]
+sorted_items = sorted(items, key=lambda x: x['price'], reverse=True)
+print(sorted_items[:3])
+
+"Пункт в"
+slov = [{'a': 1}, {'b': 2}, {'c': 3}]
+ubrat = {'a': 1}
+resc = []
+for s in slov:
+  if s != ubrat:
+    resc.append(s)
+print(resc)
