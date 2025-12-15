32372234e6984d6a1b11f293eca31da144817962
sonechk4 <sofya.pogodina.2602@gmail.com>
2025-12-15 21:55:09 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+with open("file.txt", "w") as f:
+    f.write("Первая\nВторая\nТретья\nЧетвертая\n")
+K = 2
+with open("file.txt", "r") as f:
+    lines = f.readlines()
+if K <= len(lines):
+    lines.insert(K - 1, "\n")
+    with open("file.txt", "w") as f:
+        f.writelines(lines)
+with open("file.txt", "r") as f:
+    print(f.read())
